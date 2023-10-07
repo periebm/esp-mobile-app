0b1d90f160d5757b070b69fda891995499b52d49
Pericles <periclesebmendes@gmail.com>
2023-10-07 14:43:36 -0300

    feat: basic request to esp

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,15 +1,35 @@
-import { Container } from "./styled";
+import { ButtonRequest, ButtonText, Container } from "./styled";
 import { StatusBar } from 'expo-status-bar';
-import { Button, Text } from 'react-native';
+import { Button, Pressable, Text } from 'react-native';
+import {API_KEY} from "react-native-dotenv"
+import axios from 'axios';
+import { useState } from "react";
 
 export default function Main({navigation}) {
-  
+  const api = API_KEY;
+  const [number, setNumber] = useState(0);
+
+  function handleRequest() {
+    const promise = axios.get(`${api}/randomNumber`)
+      promise.then((result) => {
+        setNumber(result.data)
+      }).catch((err) => {
+        console.log(err)
+      });
+  }
+
   return (
     <Container>
-      <Text>Main!</Text>
+      <Text>Main Page</Text>
       <StatusBar style="auto" />
+      <Text>{number}</Text>
+      <ButtonRequest
+        onPress={() => handleRequest()}
+      >
+        <ButtonText> REQUEST TO BACKEND </ButtonText>
+      </ButtonRequest>
       <Button
-        title="Go to Details"
+        title="Change Page"
         onPress={() => navigation.navigate('Details')}
       />
     </Container>
--- src/pages/Main/styled.js
+++ src/pages/Main/styled.js
@@ -6,3 +6,16 @@ export const Container = styled.View`
     align-items: center;
     justify-content: center;
   `;
+
+export const ButtonRequest = styled.Pressable`
+    background-color: green;
+    margin-top: 10px;
+    margin-bottom: 10px;
+    padding: 7px 7px;
+    border-radius: 10px;
+    
+  `
+
+export const ButtonText = styled.Text`
+color: white;
+`
